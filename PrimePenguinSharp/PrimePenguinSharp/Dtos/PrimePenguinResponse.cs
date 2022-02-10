ed96d24ad4c5ed97cef3aee591066fb2c1790164
Sumit <36700687+Khungersumit@users.noreply.github.com>
2022-02-10 13:46:48 +0530

    Created Service SDK

---
+++ PrimePenguinSharp/PrimePenguinSharp/Dtos/PrimePenguinResponse.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+
+namespace PrimePenguinSharp.Dtos
+{
+    public class PrimePenguinResponse<T>
+    {
+        [JsonProperty("result", NullValueHandling = NullValueHandling.Ignore)]
+        public T Result { get; set; }
+
+        [JsonProperty("success", NullValueHandling = NullValueHandling.Ignore)]
+        public bool Success { get; set; }
+
+        [JsonProperty("unAuthorizedRequest", NullValueHandling = NullValueHandling.Ignore)]
+        public bool UnAuthorizedRequest { get; set; }
+
+        [JsonProperty("error", NullValueHandling = NullValueHandling.Ignore)]
+        public PrimePenguinError Error { get; set; }
+    }
+
+    public class PrimePenguinError
+    {
+        [JsonProperty("code", NullValueHandling = NullValueHandling.Ignore)]
+        public bool Code { get; set; }
+
+        [JsonProperty("message", NullValueHandling = NullValueHandling.Ignore)]
+        public string Message { get; set; }
+
+        [JsonProperty("details", NullValueHandling = NullValueHandling.Ignore)]
+        public string Details { get; set; }
+
+        [JsonProperty("validationErrors", NullValueHandling = NullValueHandling.Ignore)]
+        public PrimePenguinValidationErrors ValidationErrors { get; set; }
+    }
+
+    public class PrimePenguinValidationErrors
+    {
+        [JsonProperty("message", NullValueHandling = NullValueHandling.Ignore)]
+        public string Message { get; set; }
+
+        [JsonProperty("members", NullValueHandling = NullValueHandling.Ignore)]
+        public string[] Members { get; set; }
+    }
+}
---
+++ PrimePenguinSharp/PrimePenguinSharp/PrimePenguinServiceClient/PrimePenguinService.Extension.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using Newtonsoft.Json;
+using PrimePenguinSharp.Dtos;
+
+namespace PrimePenguinSharp.PrimePenguinServiceClient
+{
+    public partial class Client
+    {
+        public string _accessToken;
+        public int _tenantId;
+
+        public Client(string baseUrl, string accessToken, int tenantId) : this(baseUrl, new HttpClient())
+        {
+            BaseUrl = baseUrl;
+            _accessToken = accessToken;
+            _tenantId = tenantId;
+        }
+
+        partial void PrepareRequest(HttpClient client, HttpRequestMessage request, StringBuilder urlBuilder)
+        {
+        }
+
+        partial void PrepareRequest(HttpClient client, HttpRequestMessage request, string url)
+        {
+            request.Headers.Add("Abp.TenantId", _tenantId.ToString());
+            request.Headers.Add("Authorization", $"Bearer {_accessToken}");
+        }
+
+        partial void ProcessResponse(HttpClient client, HttpResponseMessage response)
+        {
+        }
+
+        partial void UpdateJsonSerializerSettings(JsonSerializerSettings settings)
+        {
+            settings.NullValueHandling = NullValueHandling.Ignore;
+            settings.MissingMemberHandling = MissingMemberHandling.Ignore;
+        }
+
+        public partial class PrimePenguinServiceException : System.Exception
+        {
+            public int StatusCode { get; private set; }
+
+            public string Response { get; private set; }
+
+            public PrimePenguinResponse<dynamic> PrimePenguinResponse { get; set; }
+
+            public IReadOnlyDictionary<string, IEnumerable<string>> Headers { get; private set; }
+
+            public PrimePenguinServiceException(string message, int statusCode, string response, IReadOnlyDictionary<string, IEnumerable<string>> headers, System.Exception innerException)
+                : base(message + "\n\nStatus: " + statusCode + "\nResponse: \n" + response.Substring(0, response.Length >= 512 ? 512 : response.Length), innerException)
+            {
+                StatusCode = statusCode;
+                Response = response;
+                Headers = headers;
+                try
+                {
+                    PrimePenguinResponse = JsonConvert.DeserializeObject<PrimePenguinResponse<dynamic>>(response);
+                }
+                catch (Exception e)
+                {
+
+                }
+            }
+
+            public override string ToString()
+            {
+                return string.Format("HTTP Response: \n\n{0}\n\n{1}", Response, base.ToString());
+            }
+        }
+
+        public partial class PrimePenguinServiceException<TResult> : PrimePenguinServiceException
+        {
+            public TResult Result { get; private set; }
+
+            public PrimePenguinServiceException(string message, int statusCode, string response, Dictionary<string, IEnumerable<string>> headers, TResult result, System.Exception innerException)
+                : base(message, statusCode, response, headers, innerException)
+            {
+                Result = result;
+            }
+        }
+    }
+}
